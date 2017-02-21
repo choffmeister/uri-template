bce1e6ef1a6350763d7f399e95da1d51f37f08e2
Christian Hoffmeister <mail@choffmeister.de>
2017-02-21 19:42:50 +0100

    Crossbuild for Scala 2.11.8 and 2.12.1

--- project/Build.scala
+++ project/Build.scala
@@ -1,40 +1,25 @@
 import sbt._
 import sbt.Keys._
-import xml.Group
+import bintray.BintrayPlugin.autoImport.bintrayReleaseOnPublish
 
 object Build extends sbt.Build {
-
-  lazy val buildSettings = Defaults.defaultSettings ++ Seq(
-    organization := "no.arktekk",
-    scalaVersion := "2.11.2",
-    crossScalaVersions := Seq("2.11.2","2.10.1"),
-    publishTo <<= (version) apply {
-      (v: String) => if (v.trim().endsWith("SNAPSHOT")) Some(Resolvers.sonatypeNexusSnapshots) else Some(Resolvers.sonatypeNexusStaging)
-    },
-    pomIncludeRepository := { x => false },
-    credentials += Credentials(Path.userHome / ".sbt" / "arktekk-credentials")      
-  )
-
   lazy val root = Project(
     id = "root",
     base = file("."),
-    settings = buildSettings ++ Seq(
+    settings = Seq(
+    name := "uri-template",
       description := "URI Template",
-      name := "uri-template", 
+      organization := "no.arktekk",
+      scalaVersion := "2.11.8",
+      crossScalaVersions := Seq("2.12.1","2.11.8"),
       libraryDependencies ++= Seq(
-        "org.scalatest" %% "scalatest" % "2.2.2" % "test"
+        "org.scala-lang.modules" %% "scala-parser-combinators" % "1.0.5",
+        "org.scalatest" %% "scalatest" % "3.0.0" % "test"
       ),
-      libraryDependencies <++= (scalaBinaryVersion) apply {(sv: String) =>
-        if(sv == "2.11") Seq("org.scala-lang.modules" % "scala-parser-combinators_2.11" % "1.0.2") else Nil
-      },
-    manifestSetting
-    ) ++ mavenCentralFrouFrou
+      manifestSetting,
+      bintrayReleaseOnPublish in ThisBuild := false
+    )
   )
-
-  object Resolvers {
-    val sonatypeNexusSnapshots = "Sonatype Nexus Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots"
-    val sonatypeNexusStaging = "Sonatype Nexus Staging" at "https://oss.sonatype.org/service/local/staging/deploy/maven2"
-  }
 
   lazy val manifestSetting = packageOptions <+= (name, version, organization) map {
     (title, version, vendor) =>
@@ -51,25 +36,4 @@ object Build extends sbt.Build {
         "Implementation-Vendor" -> vendor
       )
   }
-
-  // Things we care about primarily because Maven Central demands them
-  lazy val mavenCentralFrouFrou = Seq(
-    homepage := Some(new URL("http://github.com/arktekk/uri-template")),
-    startYear := Some(2011),
-    licenses := Seq(("Apache 2", new URL("http://www.apache.org/licenses/LICENSE-2.0.txt"))),
-    pomExtra <<= (pomExtra, name, description) {(pom, name, desc) => pom ++ Group(
-      <scm>
-        <url>https://github.com/arktekk/uri-template</url>
-        <connection>scm:git:git://github.com/arktekk/uri-template.git</connection>
-        <developerConnection>scm:git:git@github.com:arktekk/uri-template.git</developerConnection>
-      </scm>
-      <developers>
-        <developer>
-          <id>teigen</id>
-          <name>Jon-Anders Teigen</name>
-          <url>http://twitter.com/jteigen</url>
-        </developer>
-      </developers>
-    )}
-  )
 }
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,10 +1,3 @@
-// IDEA plugin
-resolvers += "sbt-idea-repo" at "http://mpeltonen.github.com/maven/"
+addSbtPlugin("com.typesafe.sbt" % "sbt-pgp" % "1.0-M1")
 
-addSbtPlugin("com.github.mpeltonen" % "sbt-idea" % "1.6.0")
-
-//addSbtPlugin("no.arktekk.sbt" % "aether-deploy" % "0.9")
-
-resolvers += Resolver.url("sbt-plugin-releases", new URL("http://scalasbt.artifactoryonline.com/scalasbt/sbt-plugin-releases/"))(Resolver.ivyStylePatterns)
-
-addSbtPlugin("com.typesafe.sbt" % "sbt-pgp" % "0.8.3")
+addSbtPlugin("me.lessis" % "bintray-sbt" % "0.3.0")
